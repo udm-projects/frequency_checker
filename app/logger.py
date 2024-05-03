4d5030cad7306f5a8c4edd71179d74e3ed30cc81
udm-projects <socdemrus@gmail.com>
2024-05-03 21:29:46 +0300

    version 1.00

--- app/logger.py
+++ app/logger.py
@@ -1,13 +1,15 @@
 import logging
 from logging.handlers import TimedRotatingFileHandler
 
+from config import path_to_log
+
 logger = logging.getLogger(__name__)
 logger.setLevel('DEBUG')
 
 log_formatter = logging.Formatter('%(levelname)s (%(asctime)s) '
                                   '[%(filename)s:%(lineno)s]: %(message)s ')
 # ротация файла лога по понедельникам, суммарная длительность - 4 недели
-file_handler = TimedRotatingFileHandler(filename='logs/log.log',
+file_handler = TimedRotatingFileHandler(filename=path_to_log,
                                         encoding='utf-8',
                                         when='W0',
                                         backupCount=3)
--- config.py
+++ config.py
@@ -2,3 +2,4 @@ version = '1.00'
 
 path_to_serialization = 'data/word_dict.pickle'
 path_to_xlsx_data_source = 'data/BNC_COCA_lists.xlsx'
+path_to_log = 'logs/log.log'
